9f1dbf49776883c01958d46ce6325b381daadafc
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-01 12:14:41 +0530

    update nav bar with js

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+let text = document.querySelectorAll(".link");
+let bar = document.querySelectorAll(".menu")
+let navBar = document.querySelector("nav");
+console.log(text)
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        navBar.classList.add("scrolled");
+        text.forEach(e => e.style.color = "white")
+        // bar.style.backgroundColor = "white"
+        bar.forEach(e => e.style.backgroundColor = "white")
+
+    } else {
+        navBar.classList.remove("scrolled");
+        text.forEach(e => e.style.color = "black")
+        bar.forEach(e => e.style.backgroundColor = "black")
+    }
+});
+
